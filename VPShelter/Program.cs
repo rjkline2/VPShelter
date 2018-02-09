fcd14b7af6c1ec3998dabf81c71bdbacd3f66008
RJ Kline <rj.kline@gmail.com>
2018-02-09 11:35:38 -0500

    First Commit

---
+++ VPShelter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //nteractive user interface 
+            //Display current status of pet
+            //Display options for interacting with pet
+            //Ask user what action to take
+            int userChoice;
+
+            VirtualPet userVirtualPet = new VirtualPet(7, 4, 0, 0);
+
+            do
+            {
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Hello, welcome to the Virtual Pet app.");
+                Console.WriteLine("What would you like to do with your pet?");
+                Console.WriteLine("To feed it, type 1");
+                Console.WriteLine("To give it water, type 2");
+                Console.WriteLine("To check it's status 3");
+                Console.WriteLine("To quit, type, type 4");
+
+                userChoice = int.Parse(Console.ReadLine());
+                userVirtualPet.Tick();
+
+
+
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Currently the lil' beast is: " + userVirtualPet.Hunger);
+                        Console.WriteLine("How much food do you want to give it? Whole numbers only, between 1-8");
+                        int foodAdded = int.Parse(Console.ReadLine());
+                        userVirtualPet.AddFood(foodAdded);
+                        Console.WriteLine("Here is the beast's current hunger level:" + userVirtualPet.Hunger);
+                        break;
+                    case 2:
+                        Console.WriteLine("Currently the lil' beast's thirst level is: " + userVirtualPet.Thirst);
+                        Console.WriteLine("How much water do you want to give it? Whole numbers only, between 1-8");
+                        int waterAdded = int.Parse(Console.ReadLine());
+                        userVirtualPet.AddWater(waterAdded);
+                        Console.WriteLine("Here is the beast's current water level:" + userVirtualPet.Thirst);
+                        break;
+                    case 3:
+                        Console.WriteLine("The beast is currently:");
+                        Console.WriteLine("Hunger level: " + userVirtualPet.Eat());
+                        Console.WriteLine("Thirst level: " + userVirtualPet.Drink());
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (userChoice != 4);
+        }
+    }
+}
---
+++ VPShelter/VirtualPet.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class VirtualPet
+    {
+
+        //fields == baby bears
+        private int hunger;
+        private int thirst;
+        private int feeding;
+        private int watering;
+
+
+        //Properties == Mama bear get/set.......get allows us to see the value...set allows us to assign the value...pascal
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+        public int Feeding
+        {
+            get { return this.feeding; }
+            set { this.feeding = value; }
+        }
+        public int Watering
+        {
+            get { return this.watering; }
+            set { this.watering = value; }
+        }
+
+
+        //Constructors
+        public VirtualPet()
+        {
+
+        }
+        public VirtualPet(int hunger, int thirst, int feeding, int watering)
+        {
+
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.feeding = feeding;
+            this.watering = watering;
+
+
+        }
+        //Methods()
+        //at least three
+        //one must be tick
+        public string Eat()
+        {
+            if (hunger >= 8)
+            {
+                return "Beast doesn't need fed!";
+            }
+            else if (hunger > 4)
+            {
+                return "getting hungry";
+            }
+            else
+            {
+                return "time to feed the beast!";
+            }
+        }
+
+        public void Tick()
+        {
+            hunger = hunger - 2;
+            thirst = thirst - 2;
+        }
+        public string Drink()
+        {
+            if (thirst >= 8)
+            {
+                return "Beastie doesn't need water!";
+            }
+            else if (thirst > 4)
+            {
+                return "getting thirsty";
+            }
+            else
+            {
+                return "Wee beastie needs water!";
+            }
+        }
+        public void AddFood(int foodAdded)
+        {
+            hunger += foodAdded;
+        }
+        public void AddWater(int waterAdded)
+        {
+            thirst += waterAdded;
+        }
+
+    }
+}
