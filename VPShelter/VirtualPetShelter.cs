8d2beb004d0e934aa2b2a0da595b063371347619
RJ Kline <rj.kline@gmail.com>
2018-02-09 15:24:01 -0500

    Project Update

---
+++ VPShelter/VirtualPetShelter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class VirtualPetShelter
+    {
+        //
+        List<string> petList = new List<string>() { "Maurice", "Chauncey", "Lockheed" };
+        List<string> empList = new List<string>() { "Fred", "Velma", "Nardo" };
+        
+    }
+}
