fc24c2dd752df4c7f2bdeef8024dda12b44ad57a
RJ Kline <rj.kline@gmail.com>
2018-02-11 16:57:36 -0500

    Project Update

---
+++ VPShelter/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    class Manager : Employee
+    {
+        //fields
+        //properties
+        //empid? Salary?
+        //constructors
+        //methods
+        //adopt a pet
+        //feed a pet
+        //water a pet
+        //clockin override?
+    }
+}
